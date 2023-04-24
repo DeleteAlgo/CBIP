afbb9cc549d93e2e50ca3bd1960c36fa6222448c
Ronald Rasco <120026829+DeleteAlgo@users.noreply.github.com>
2023-04-24 17:48:23 +0800

    🎉 Initial Commit

---
+++ CBIP/js/index.js
@@ -0,0 +1,49 @@
+// const container = document.querySelector('.logo-marq-container');
+// const content = document.querySelector('.logo-marq');
+
+// const scroll = () => {
+//   if (container.scrollLeft + container.clientWidth >= container.scrollWidth) {
+//     container.scrollLeft = 0;
+//   } else {
+//     container.scrollLeft += 1;
+//   }
+// }
+
+// setInterval(scroll, 100);
+
+const accordionHeaders = document.querySelectorAll(".faq-header");
+accordionHeaders.forEach((header) => {
+  header.addEventListener("click",(event) => {
+    const accordionItem = event.target.parentNode;
+    accordionItem.classList.toggle("active");
+    if(accordionItem.classList.contains("active")){
+      event.target.querySelector('i').setAttribute('class','fa-solid fa-caret-down')
+    }else {
+      event.target.querySelector('i').setAttribute('class','fa-solid fa-caret-right')
+    }
+  })
+})
+
+var prevScrollpos = "700";
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if(window.innerWidth <= 767) {
+    if (1400 >= currentScrollPos) {
+      document.querySelector(".custom-nav").style.top = "-180px";
+      document.getElementById('up-btn').style.display = "none";
+    } else {
+      document.querySelector(".custom-nav").style.top = "0";
+      document.getElementById('up-btn').style.display = "block";
+    }
+  }else{
+    if (prevScrollpos >= currentScrollPos) {
+      document.querySelector(".custom-nav").style.top = "-180px";
+    } else {
+      document.querySelector(".custom-nav").style.top = "0";
+    }
+    // prevScrollpos = currentScrollPos;
+  }
+}
+
+
+
